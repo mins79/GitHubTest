601ed0e0883a2c4ce13688f73656767afcf25e9f
mins79 <mins79@gmail.com>
2018-01-11 15:22:53 +0900

    두번째 2-3

--- app/src/main/java/com/mins/githubtest/MainActivity.java
+++ app/src/main/java/com/mins/githubtest/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // 1-1
-    // 2-2
+    // 2-3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
