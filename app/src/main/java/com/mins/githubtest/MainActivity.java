c5143906f42aada37013a3bebf28217e7783f374
mins79 <mins79@gmail.com>
2018-01-11 16:35:09 +0900

    첫번째 1-15

--- app/src/main/java/com/mins/githubtest/MainActivity.java
+++ app/src/main/java/com/mins/githubtest/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // 1-14
+    // 1-15
     // 2-11
     // 3-3
     @Override
