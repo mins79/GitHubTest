115a1d91f2fdd3549fd98a14c80bd4829f8d7bc2
mins79 <mins79@gmail.com>
2018-01-11 15:32:13 +0900

    첫번째 1-9

--- app/src/main/java/com/mins/githubtest/MainActivity.java
+++ app/src/main/java/com/mins/githubtest/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // 1-8
+    // 1-9
     // 2-7
     @Override
     protected void onCreate(Bundle savedInstanceState) {
